75ee4c534c466f8c3b7b6aec479484a9bd524fef
uerbzr <nigel.sibbert@gmail.com>
2023-03-09 18:33:43 +0000

    Add Extension Test

--- csharp-scrabble-challenge-exemplar.Test/ScrabbleTestExtension.cs
+++ csharp-scrabble-challenge-exemplar.Test/ScrabbleTestExtension.cs
@@ -11,14 +11,14 @@ namespace csharp_scrabble_challenge_exemplar.Test
     [TestFixture]
     public class ScrabbleTestExtension
     {
-        /*
+
         [Test]
         public void Extension_shouldScore10forZasTriple()
         {
-          
+
             Scrabble scrabble = new Scrabble("{z}");
             Assert.AreEqual(10, scrabble.score());
         }
-        */
+
     }
 }
