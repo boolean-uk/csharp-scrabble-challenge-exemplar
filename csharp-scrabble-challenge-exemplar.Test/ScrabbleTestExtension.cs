02e20db612e592d4d41f7d17e62d676bac169f08
uerbzr <nigel.sibbert@gmail.com>
2023-03-10 09:39:12 +0000

    Some refactor and comments

--- csharp-scrabble-challenge-exemplar.Test/ScrabbleTestExtension.cs
+++ csharp-scrabble-challenge-exemplar.Test/ScrabbleTestExtension.cs
@@ -11,8 +11,14 @@ namespace csharp_scrabble_challenge_exemplar.Test
     [TestFixture]
     public class ScrabbleTestExtension
     {
+        [Test]
+        [TestCase("street", 6)] //TODO: change this to test triple word score
+        public void WordScoreTests(string word, int targetScore)
+        {
+            Assert.AreEqual(this.GetWordScore(word), targetScore);
+        }
 
-       
+        private int GetWordScore(string word) => new Scrabble(word).score();
 
     }
 }
